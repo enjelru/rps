0aa77412062456fe27088ac04e81eeed4e4ee2f9
enjelru <139509825+enjelru@users.noreply.github.com>
2024-06-13 21:50:12 +0800

    remove unwanted comments

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function playGame() {
     playRound(playerSelection, computerSelection);
     return "\u{1F44A}";
   })
-  
+
   paper.addEventListener("click", () => {
     console.log("you choose paper");
     showWinner.textContent = "";
@@ -63,10 +63,9 @@ function playRound(playerChoice, computerChoice) {
     plChoice.textContent = `${playerChoice}`;
     pcChoice.textContent = `${computerChoice}`;
     display.textContent = `It's a tie! ${playerChoice} against ${computerChoice}.`;
-    // return "tie"
 
   } else if (playerChoice === "\u{1F44A}" && computerChoice === "\u270c" ||
-    (playerChoice === "\u270c" && computerChoice === "\u{1f590}") || 
+    (playerChoice === "\u270c" && computerChoice === "\u{1f590}") ||
     (playerChoice === "\u{1f590}" && computerChoice === "\u{1F44A}")
   ) {
     ++playerScore;
@@ -75,29 +74,21 @@ function playRound(playerChoice, computerChoice) {
     plChoice.textContent = `${playerChoice}`;
     pcChoice.textContent = `${computerChoice}`;
     display.textContent = `You Win! ${playerChoice} beats ${computerChoice}.`;
-    // return "win";
   } else {
     ++computerScore;
     console.log(`You lose! ${playerChoice} against ${computerChoice}. Your score: ${playerScore}, Computer score: ${computerScore}`);
     plChoice.textContent = `${playerChoice}`;
     pcChoice.textContent = `${computerChoice}`;
     display.textContent = `You lose! ${playerChoice} against ${computerChoice}`;
-    // return "lose";
   }
 
   playerResult.textContent = playerScore;
   computerResult.textContent = computerScore;
-  // try use loop
   if (playerScore === 5 || computerScore === 5) {
     gameWinner(playerScore, computerScore);
   }
-  // for (let i = 0; i < 5; i++) {
-  //   gameWinner(playerScore, computerScore);
-  // }
 }
 
-
-//invoke function resetGame
 function resetGame() {
   playerScore = 0;
   computerScore = 0;
@@ -108,26 +99,22 @@ function gameWinner(playerScore, computerScore) {
 
   if (playerScore === 5) {
     console.log("Congratulations, You win the game! " + "Player score: " + playerScore);
-    // showWinner.setAttribute("style", "padding: 0 0 0 0;")
-    // showWinner.textContent = "Congratulations, You win the game! " + "Player score: " + playerScore;
   } else if (computerScore === 5) {
     console.log("You lose the game! " + "Computer score: " + computerScore);
-    // showWinner.textContent = "You lose the game! " + "Computer score: " + computerScore;
-    }
-    
+  }
+
   display.textContent = "";
   resetGame();
   playAgain(playerScore, computerScore);
-  
+
 }
 
-// invoke play again
 function playAgain(playerWins, computerWins) {
 
   const message = playerWins === 5 ? "Congratulations, You win the game! " : "You lose the game!";
-  
+
   const playAgainBox = document.querySelector("#playAgain");
-  const firstContainer= document.createElement("div");
+  const firstContainer = document.createElement("div");
   firstContainer.setAttribute("class", "modal");
 
   const modal = document.createElement("div");
@@ -139,32 +126,19 @@ function playAgain(playerWins, computerWins) {
   paraWinner.setAttribute("style", "font-size: 3rem; padding-bottom: 0.5rem;");
   firstContainer.appendChild(paraWinner);
 
-  // if(playerWins === 5) {
-  //   const paraWinner = document.createElement("p");
-  //   paraWinner.textContent = "Congratulations, You win the game! ";
-  //   firstContainer.appendChild(paraWinner);
-  // } else if (computerWins === 5) {
-  //   const paraLoser = document.createElement("p");
-  //   paraLoser.textContent = "You lose the game! ";
-  //   firstContainer.appendChild(paraLoser);
-  // }
-
   const para = document.createElement("p");
   para.textContent = "Play again?";
   para.setAttribute("style", "color: rgb(194, 43, 131); text-align: center; padding-bottom: 10px; margin: 0");
   modal.appendChild(para);
-  // firstContainer.appendChild(para);
-  
+
   const yesBtn = document.createElement("button");
   yesBtn.classList.add("playAgainBtn");
   yesBtn.textContent = "Yes";
   modal.appendChild(yesBtn);
-  // firstContainer.appendChild(yesBtn);
 
   const noBtn = document.createElement("button");
   noBtn.classList.add("playAgainBtn");
   noBtn.textContent = "No";
-  // firstContainer.appendChild(noBtn);
   modal.appendChild(noBtn);
 
   firstContainer.appendChild(modal);
@@ -175,8 +149,8 @@ function playAgain(playerWins, computerWins) {
     playAgainBox.removeChild(firstContainer);
 
     showWinner.textContent = "Game reset";
-    playerResult.textContent ="0";
-    computerResult.textContent ="0";
+    playerResult.textContent = "0";
+    computerResult.textContent = "0";
     plChoice.textContent = "";
     pcChoice.textContent = "";
     display.textContent = "Choose the following:";
@@ -189,8 +163,8 @@ function playAgain(playerWins, computerWins) {
     playAgainBox.removeChild(firstContainer);
 
     showWinner.textContent = "Game over! Thank you for playing!";
-    playerResult.textContent ="0";
-    computerResult.textContent ="0";
+    playerResult.textContent = "0";
+    computerResult.textContent = "0";
     plChoice.textContent = "";
     pcChoice.textContent = "";
     display.textContent = "Start the game!";
